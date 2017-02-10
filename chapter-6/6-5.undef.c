56221bdf4554c658a9d0d43aa285404f79f92321
sandii <chenzhibupt@qq.com>
2017-02-10 22:18:05 +0800

    update

--- chapter-6/6-5.undef.c
+++ chapter-6/6-5.undef.c
@@ -6,6 +6,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h> // malloc free
 #include <string.h>	// strcmp strlen strcpy
 
 struct nlist {
@@ -36,26 +37,65 @@ struct nlist* get (char* key) {
 struct nlist* set (char* key, char* val) {
 	struct nlist* np = get(key);
 
-	// no node
+	// not found
 	if (np == NULL) {
-		np = (struct nlist*) malloc(sizeof struct nlist);
+		np = (struct nlist*) malloc(sizeof (struct nlist));
 		np -> key = (char*) malloc(strlen(key) + 1);
-		np -> val = (char*) malloc(strlen(key) + 1);
 		strcpy(np -> key, key);
-		strcpy(np -> val, val);
 		unsigned hashVal = hash(key);
 		np -> next = hashtab[hashVal];
 		hashtab[hashVal] = np;
+
+	// already there
+	} else {
+		free((void*) np -> val);
 	}
-	// node exist
+	np -> val = (char*) malloc(strlen(val) + 1);
+	strcpy(np -> val, val);
 	return np;
 }
 
+bool undef (char* key) {
+	unsigned hashVal = hash(key);
+	struct nlist* prev = NULL;
+	struct nlist* curr = hashtab[hashVal];
+	while (curr != NULL) {
+		if (strcmp(key, curr -> key) == 0) break;
+		prev = curr;
+		curr = curr -> next;
+	}
+	// not found
+	if (curr == NULL) return false;
 
+	// is root of link-list
+	if (prev == NULL) {
+		hashtab[hashVal] = NULL;
+	} else {
+		prev -> next = curr -> next;
+	}
+
+	free((void*) curr -> key);
+	free((void*) curr -> val);
+	free((void*) curr);
+	return true;
+}
 
 main () {
 	struct nlist* lp = NULL;
-	set("size", "100");
-	lp = get("size");
+	char key[] = "size";
+	char ten[] = "10";
+	char thirteen[] = "13";
+	char hundred[] = "100";
+	char thousand[] = "1000";
+
+	set(key, ten);
+	lp = get(key);
 	printf("%s\n", lp -> val);
+
+	set(key, thirteen);
+	printf("%s\n", lp -> val);
+
+	undef(key);
+	lp = get(key);
+	printf("%d\n", lp == NULL);
 }
