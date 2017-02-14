c592a78348f0405ac2e08ace4b8f282565f23afb
sandii <chenzhibupt@qq.com>
2017-02-14 16:07:08 +0800

    update

--- chapter-7/7-3.minprintf.c
+++ chapter-7/7-3.minprintf.c
@@ -3,12 +3,15 @@
 * data: Feb 14, 2017
 *
 * simplified printf
+*
+* pratically, sprintf is much better than hand-written itoa / ftoa 
 */
 
 #include <stdio.h>
 #include <stdlib.h>	// abs
 #include <string.h>	// strlen
 #include <math.h>	// pow
+#include <ctype.h>	// isdigit
 #include <stdarg.h>
 
 #define MAX_BUFF_LEN 100
@@ -57,6 +60,7 @@ void ftoa (double f, char s[]) {
 	f = f - (double) i;
 	i = (int) ceil(f * pow(10, PRECISE));
 	while (i % 10 == 0) {
+		if (i == 0) break;
 		i /= 10;
 	}
 	itoa(i, fs);
@@ -75,6 +79,33 @@ void ftoa (double f, char s[]) {
 	*sp = '\0';
 }
 
+void adjustPrecise (int n, char s[]) {
+	if (n == 0) return;
+	char* sp = s;
+	for (sp = strchr(s, '.'); *sp != '\0'; sp++) {
+		if (n-- < 0) break;
+	};
+	while (n-- >= 0) {
+		*sp++ = '0';
+	}
+	*sp = '\0';
+}
+
+void adjustWidth (int n, char s[]) {
+	if (n == 0) return;
+	int len = strlen(s);
+	if (n <= len) return;
+
+	int reader = len;
+	int writer = n;
+	while (reader <= 0) {
+		s[writer--] = s[reader--];
+	}
+	for (writer = 0; writer < n - len; writer++) {
+		s[writer] = ' ';
+	}
+}
+
 int minprintf (char* fmp, ...) {
 	va_list ap;
 	va_start(ap, fmp);
@@ -87,15 +118,31 @@ int minprintf (char* fmp, ...) {
 			putchar(*wp); len++;
 			continue;
 		}
-		switch (*++wp) {
+
+		int minWidth = 0;
+		while (isdigit(*++wp)) {
+			minWidth = minWidth * 10 + (*wp - '0');
+		}
+
+		int precise = 0;
+		if (*wp == '.') {
+			while (isdigit(*++wp)) {
+				precise = precise * 10 + (*wp - '0');
+			}
+		}
+
+		switch (*wp) {
 			case 'd':
 				itoa(va_arg(ap, int), buff);
+				adjustWidth(minWidth, buff);
 				for (sp = buff; *sp != '\0'; sp++) {
 					putchar(*sp); len++;
 				}
 				break;
 			case 'f':
 				ftoa(va_arg(ap, double), buff);
+				adjustPrecise(precise, buff);
+				adjustWidth(minWidth, buff);
 				for (sp = buff; *sp != '\0'; sp++) {
 					putchar(*sp); len++;
 				}
@@ -114,6 +161,7 @@ int minprintf (char* fmp, ...) {
 }
 
 main () {
-	int n = minprintf("%s %d, %f\n", "April", 10, -19.88);
+	// int n = minprintf("%s %10d, %.4f, %10.2f\n", "April", 10, -1988.0, 6.6666);
+	int n = minprintf("-%4d-\n", 10);
 	minprintf("%d\n", n);
 }
