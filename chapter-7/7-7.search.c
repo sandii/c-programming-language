1b867db6ee392f216acc9f10f6d4b2b115cebf07
sandii <chenzhibupt@qq.com>
2017-02-15 20:30:09 +0800

    update

--- chapter-7/7-7.search.c
+++ chapter-7/7-7.search.c
@@ -11,46 +11,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>	// exit
-#include <string.h>	// strcmp strlen
+#include <string.h>	// strstr strlen
 
 #define MAX_LINE_LEN 200
-char line1[MAX_LINE_LEN];
-char line2[MAX_LINE_LEN];
+char line[MAX_LINE_LEN];
 
-int getFileLine (char* line, int max, FILE* fp) {
-	char* sp = fgets(line, max, fp);
-	if (sp == NULL) return 0;
+int getFileLine (FILE* fp) {
+	if (fgets(line, MAX_LINE_LEN, fp) == NULL) return 0;
 	return strlen(line);
 }
 
-int compare (FILE* fp1, FILE* fp2) {
+int search (char* pattern, char* filename) {
+	FILE* fp = fopen(filename, "r");
+	if (fp == NULL) {
+		printf("cannot open %s\n", filename);
+		return 0;
+	}
+	int len = 0;
 	int lineNo = 1;
-	int len1 = 0;
-	int len2 = 0;
-	do {
-		len1 = getFileLine(line1, MAX_LINE_LEN, fp1);
-		len2 = getFileLine(line2, MAX_LINE_LEN, fp2);
-		if (strcmp(line1, line2) != 0) return lineNo;
-		lineNo++;
-	} while (len1 != 0 && len2 != 0);
+	int colmNo = 1;
+	while (len = getFileLine(fp)) {
+		char* cp = strstr(line, pattern);
+		if (cp == NULL) {
+			lineNo++;
+			continue;
+		}
+		colmNo = cp - line + 1;
+		printf("Found pattern in file %s, at line %d, column %d.\n", filename, lineNo, colmNo);
+		return 1;
+	}
+	fclose(fp);
 	return 0;
 }
 
 main (int argc, char* argv[]) {
 	if (argc < 3) return 1;
-	FILE* fp1 = fopen(argv[1], "r");
-	FILE* fp2 = fopen(argv[2], "r");
-	if (fp1 == NULL) { printf("open %s fail..\n", argv[1]); exit(1); }
-	if (fp2 == NULL) { printf("open %s fail..\n", argv[2]); exit(1); }
 
-	int lineNo = compare(fp1, fp2);
-	if (lineNo == 0) {
-		printf("No difference!\n");
-	} else {
-		printf("Line %d is different:\n", lineNo);
-		printf("%s: %s", argv[1], line1);
-		printf("%s: %s", argv[2], line2);
+	int found = 0;
+	char* pattern = argv[1];
+	for (int i = 2; i < argc; i++) {
+		char* filename = argv[i]; 
+		found += search(pattern, filename);
 	}
-	fclose(fp1);
-	fclose(fp2);
+	if (found == 0) printf("nothing found..\n");
 }
